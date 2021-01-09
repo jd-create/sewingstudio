f0d2b66d8d1acc4d054d8d46c950ced37e27483b
jd-create <jenniferdemeijer@t-online.de>
2021-01-09 15:30:18 +0100

    First commit

---
+++ src/main/java/nl/pianoatelierdemeijer/sewingstudio/SewingstudioApplication.java
@@ -0,0 +1,13 @@
+package nl.pianoatelierdemeijer.sewingstudio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SewingstudioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SewingstudioApplication.class, args);
+    }
+
+}
---
+++ src/test/java/nl/pianoatelierdemeijer/sewingstudio/SewingstudioApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.pianoatelierdemeijer.sewingstudio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SewingstudioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
